44bb3fe847402256ff24ff65ce14c84d91391c7c
heresy0-0y <heresy.y.ysereh@gmail.com>
2022-05-02 10:56:11 -0500

    testStatic file

--- client/src/components/Songs.tsx
+++ client/src/components/Songs.tsx
@@ -5,7 +5,7 @@ import dynamic from "next/dynamic";
 
 // , {method: 'GET', cacheControl: 'max-age=31536000', mode: 'no-cors', site: 'cross-site', redirect: 'follow', referrerPolicy: 'strict-origin-when-cross-origin', accept: '*/*', dest: 'audio'}
 const testUrl = "https://static.inaturalist.org/sounds/342100.m4a";
-// const testUrl='/342100.mp4'
+const testStatic='/342100.mp4'
 const Songs = () => {
   const Waveform = dynamic(() => import("./Waveform"), { ssr: false });
 
